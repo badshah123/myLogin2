fa9c4fc2decd9e4da7d297d43a10a0124f13c1b5
badshah123 <ibrahimbadushah123@gmail.com>
2016-09-17 11:36:06 +0530

    Initial commit added

---
+++ app/src/main/java/com/example/student/mylogin2/Login.java
@@ -0,0 +1,15 @@
+package com.example.student.mylogin2;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+/**
+ * Created by Student on 17-09-2016.
+ */
+public class Login extends AppCompatActivity{
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.login);
+    }
+}
